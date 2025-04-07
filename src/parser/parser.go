e065c464b0c8f9821462d153641b008f6776f8d4
rajikak <nrajikak@gmail.com>
2025-04-07 22:58:04 +0000

    Update file(s) from rajikak/query-parser

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"fmt"
 	"query-parser/lexer"
+	"slices"
 )
 
 type Parser struct {
@@ -16,10 +17,10 @@ func New(input string) Parser {
 	return Parser{l, l.NextToken(), l.NextToken()}
 }
 
-func (p Parser) Parse() error {
+func (p *Parser) Parse() error {
 	err := p.match(lexer.ParamStartOp)
 	if err != nil {
-		return err // syntax error
+		return fmt.Errorf("syntax error: should start with %s", lexer.ParamStartOp)
 	}
 	for {
 
@@ -50,50 +51,57 @@ func (p Parser) Parse() error {
 				return fmt.Errorf("syntax error: unknown operator: %s", tok)
 			}
 		}
-
-		//if tok.Type == lexer.Keyword {
-		//	err = p.match(lexer.AssignOp)
-		//	if err != nil {
-		//		return err // syntax error
-		//	}
-		//	tok = p.nextToken()
-		//
-		//} else if tok.Type == lexer.Include {
-		//	err = p.match(lexer.AssignOp)
-		//	if err != nil {
-		//		return err // syntax error
-		//	}
-		//}
 	}
-
 	return nil
 }
 
-func (p Parser) nextToken() lexer.Token {
+func (p *Parser) nextToken() lexer.Token {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 	return p.curToken
 }
 
-func (p Parser) checkToken(typ lexer.TokenType) bool {
+func (p *Parser) checkToken(typ lexer.TokenType) bool {
 	return p.curToken.Type == typ
 }
 
-func (p Parser) checkPeek(typ lexer.TokenType) bool {
+func (p *Parser) checkPeek(typ lexer.TokenType) bool {
 	return p.peekToken.Type == typ
 }
 
-func (p Parser) match(typ lexer.TokenType) error {
+func (p *Parser) match(typ lexer.TokenType) error {
 	if !p.checkToken(typ) {
 		return fmt.Errorf("syntax error: expected: %s, got: %s", typ, p.curToken)
 	}
 	return nil
 }
 
-func (p Parser) handleFilter() {
-	fmt.Println("handle filter")
+func (p *Parser) function(tok string) bool {
+	return slices.Contains([]string{lexer.LessThan, lexer.LessOrEqual, lexer.GreaterThan, lexer.GreaterOrEqual, lexer.Contains, lexer.Contains, lexer.StartWith, lexer.EndsWith, lexer.And, lexer.Has, lexer.Not, lexer.Or, lexer.And}, tok)
 }
 
-func (p Parser) handleInclude() {
+func (p *Parser) handleFilter() error {
+	for {
+		tok := p.nextToken()
+
+		if tok.Type == lexer.Illegal {
+			// syntax error
+			return fmt.Errorf("syntax error: illegal chracter received:%s", tok)
+		}
+
+		if tok.Type == lexer.EndOfInput {
+			break
+		}
+
+		if !p.function(tok.Literal) {
+			return fmt.Errorf("syntax error: function defintion is required, got: %s", tok)
+		}
+
+	}
+
+	return nil
+}
+
+func (p *Parser) handleInclude() {
 	fmt.Println("handle include")
 }
